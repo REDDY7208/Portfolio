c0fe620b85b2c8d8a43b9247212bbdbc57fcb20b
Praveen kumar reddy <111781939+REDDY7208@users.noreply.github.com>
2026-04-16 17:15:02 +0000

    Add Google AdSense script to head

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,6 +23,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2338863848634036"
+     crossorigin="anonymous"></script>
+      </head>
       <body
         className={`${sora.variable} antialiased min-h-screen lg:h-screen overflow-x-hidden lg:overflow-hidden bg-background text-text font-sora`}
       >
